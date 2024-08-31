78b0e1e7ed5cbec13c9f9fec4719c8b7ae4d4af9
Ashish <90548985+aashxs@users.noreply.github.com>
2024-08-31 10:54:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+// //your JS code here. If required.
+// https://www.svgrepo.com/show/345221/three-dots.svg
+let btn=document.getElementById("btn");
+let fname=document.getElementById("f_name");
+let lname=document.getElementById("l_name");
+let phone=document.getElementById("phone_no");
+let mail=document.getElementById("email");
+
+btn.addEventListener("click",()=>{
+	alert(`First Name:${fname.value} Last Name:${lname.value} Phone Number:${phone.value} Email ID:${mail.value}`);
+	
+})
